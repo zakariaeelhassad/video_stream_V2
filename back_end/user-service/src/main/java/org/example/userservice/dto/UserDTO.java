ebb16dda83e3521d536089010375f9ae0a47ba37
Zakariae El Hassad <zakariaelhassad031@gmail.com>
2026-03-12 18:02:24 +0000

    ajouter l'enregistrement UserDTO simple

---
+++ back_end/user-service/src/main/java/org/example/userservice/dto/UserDTO.java
@@ -0,0 +1,17 @@
+package org.example.userservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+}
